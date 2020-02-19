8d898ad38f99524d1b343ad8d78080bcbb01fd9b
xinyi-tony-liu <lxy952@gmail.com>
2020-02-19 13:56:50 -0500

    finish tree building

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ def loadlabels(file):
             r.append(int(line.strip()))
         return r
 
-testlabels = loadlabels(r".\\datasets\\testLabel.txt")
-trainlabels = loadlabels(r".\\datasets\\trainLabel.txt")
+testlabel = loadlabels(r".\\datasets\\testLabel.txt")
+trainlabel = loadlabels(r".\\datasets\\trainLabel.txt")
 
 testdata = [np.zeros(_gwordslen) for col in range(1501)]
 traindata = [np.zeros(_gwordslen) for col in range(1501)]
@@ -36,7 +36,6 @@ def loaddata(vector, file):
 
 loaddata(testdata, r".\\datasets\\testData.txt")
 loaddata(traindata, r".\\datasets.\\trainData.txt")
-
 def I(E):
     if (len(E) == 0):
         return 1
@@ -52,18 +51,22 @@ def I(E):
 def AIG(E, E1, E2):
     return I(E) - (0.5 * I(E1) + 0.5 * I(E2))
 
-def IGW(E, E1, E2):
+def WIG(E, E1, E2):
     return I(E) - ((len(E1) -1) / len(E) * I(E1) + len(E2) / len(E) * I(E2))
 
 
 class Node:
-    def __init__(self, data, label, words, fn, w, wo):
+    def __init__(self, data, label, words, fn):
         self.data = data
         self.label = label
         self.words = words
-        self.w = w
-        self.wo = wo
+        self.fn = fn
+        self.end = 0
+        self.word = ""
+        self.weight = 0
         self.estimate = 1
+        self.ychild = None
+        self.nchild = None
         if(label.count(1) < label.count(2)):
             self.estimate = 2
 
@@ -71,10 +74,12 @@ class Node:
         maxword = ""
         if(label.count(1) != 0 and label.count(2) != 0):
             for word in words:
-                for file in range(1,1501):
-                    nw = []
-                    nwo = []
-                    if(data[file][words.index(word)] == 1):
+                nw = []
+                nwo = []
+                pos = _gwords.index(word)
+                l = len(label)
+                for file in range(l):
+                    if(data[file][pos] == 1):
                         nw.append(label[file])
                     else:
                         nwo.append(label[file])
@@ -84,19 +89,93 @@ class Node:
                     maxword = word
             self.weight = maxweight
             self.word = maxword
+        else:
+            self.end = 1
+            
+    def split(self):
+        pos = _gwords.index(self.word)
+        l = len(self.label)
+        words = self.words.copy()
+        del words[words.index(self.word)]
+        n1data = []
+        n2data = []
+        n1label = []
+        n2label = []
+        for file in range (l):
+            if (self.data[file][pos] == 1):
+                n1data.append(self.data[file])
+                n1label.append(self.label[file])
+            else:
+                n2data.append(self.data[file])
+                n2label.append(self.label[file])
+                
+        n1 = Node(n1data,n1label,words,self.fn)
+        n2 = Node(n2data,n2label,words,self.fn)
+        self.ychild = n1
+        self.nchild = n2
+        return n1, n2
+    
+    def test(self, data, label):
+        count = 0
+        l = len(label)
+        for file in range(l):
+            node = self
+            for i in range(100):
+                if(node.end == 1):
+                    break
+                pos = _gwords.index(node.word)
+                if (data[file][pos] == 1):
+                    if(node.ychild == None):
+                        break
+                    node = node.ychild
+                else:
+                    if(node.nchild == None):
+                        break
+                    node = node.nchild
+            if (node.estimate == label[file]):
+                count = count + 1
+        return count / l
+            
 
     def __gt__(self, other):
         return self.weight < other.weight
 
     def __eq__(self, other):
+        if(other == None):
+            return False
         return self.weight == other.weight
 
     def __lt__(self, other):
         return self.weight > other.weight
 
-def dt():
+def dt(fn, fname):
     q = PriorityQueue()
-    n = Node(traindata, trainlabels, _gwords, AIG, [], [])
-    print(n.word, n.weight)
+    root = Node(traindata, trainlabel, _gwords, fn)
+    q.put(root)
+    y1 = []
+    y2 = []
+    for i in range(100):
+        y1.append(root.test(traindata, trainlabel))
+        y2.append(root.test(testdata, testlabel))
+        n = q.get()
+        print(i, n.word, n.weight, n.estimate)
+        if(n.end == 1):
+            break
+        n1, n2 = n.split()
+        q.put(n1)
+        q.put(n2)
+    x = range(100)
+    plt.plot(x, y1, label = "train accuracy")
+    plt.plot(x, y2, label = "test accuracy")
+    plt.xlabel("number of nodes")
+    plt.ylabel("% correct")
+    plt.title("Accuracy when using " + fname)
+    plt.legend()
+    plt.show()
+    qcontent = []
+    while(not q.empty()):
+        qcontent.append(q.get().word)
+    print(qcontent)
 
-dt()
+dt(AIG, "Average Information Gain")
+dt(WIG, "Weighted Information Gain")
